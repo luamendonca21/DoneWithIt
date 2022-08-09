204030d8d3b28a7acc0ecb2dfaf989766811de3f
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-09 12:25:28 +0100

    render local or static images

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   const handleCliked = () => {
     console.log("text pressed");
   };
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text numberOfLines={3} onPress={handleCliked}>
         Hello React Native - A really really long text. Now i wanna make this
         even longer and see what happens.
       </Text>
+      <Image style={styles.image} source={require("./assets/icon.png")} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
