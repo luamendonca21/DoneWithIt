dc29179442329b3b915571bd98a24884a860161b
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-09 22:34:05 +0100

    borders

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 10,
+          borderTopWidth: 20,
+          borderTopLeftRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
