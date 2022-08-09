d36a10da5515a67bef4db210ef2083a1e57c558d
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-09 15:10:19 +0100

    flexbox

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  View,
-  TouchableNativeFeedback,
-  Image,
-  SafeAreaView,
-  Alert,
-  StatusBar,
-  Button,
-  Platform,
-} from "react-native";
-
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  console.log(useDimensions());
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-        }}
-      ></View>
-    </SafeAreaView>
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
+      <View style={{ backgroundColor: "dodgerblue", flex: 1 }} />
+      <View style={{ backgroundColor: "gold", flex: 1 }} />
+      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
