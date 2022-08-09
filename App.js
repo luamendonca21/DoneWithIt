7ffe0c6daa5e5afed5fc2fe0f0106c9e231247e6
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-09 12:44:40 +0100

    touchable

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello React Native</Text>
-      <Image
-        blurRadius={1}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        <Image
+          blurRadius={1}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
