e314423cc1b78277a49310cda1302698be3d9a71
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-11 00:02:07 +0100

    status bar overflow fixed

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { FlatList } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar,
+} from "react-native";
 import ListItem from "../components/ListItem";
 
 const messages = [
@@ -19,18 +25,26 @@ const messages = [
 
 const MessagesScreen = () => {
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(message) => message.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.description}
-          image={item.image}
-        />
-      )}
-    />
+    <SafeAreaView style={styles.screen}>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
 export default MessagesScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
