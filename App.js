33a35f8f8d3d2ed1c7fe2a6ed8c31cf9b8020c4e
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-12 16:43:37 +0100

    learning Switch component from react native

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, Switch, TextInput, View } from "react-native";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -11,12 +11,17 @@ import MessagesScreen from "./app/screens/MessagesScreen";
 
 import Screen from "./app/components/Screen";
 import AppText from "./app/components/AppText";
-import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
+  const [isNew, setIsNew] = useState(false);
   return (
     <Screen>
-      <AppTextInput placeholder="FirstName" icon="email" />
+      <Switch
+        thumbColor={isNew ? "#fc5c65" : "#6e6969"}
+        trackColor={{ false: "#6e6969", true: "#fc5c65" }}
+        value={isNew}
+        onValueChange={(newValue) => setIsNew(newValue)}
+      />
     </Screen>
   );
 }
