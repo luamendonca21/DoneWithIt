9f72db18eb4a5b8d6c9b9360f9b0ce02cd820780
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-12 22:25:27 +0100

    library formik to improve the form component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,48 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, StyleSheet, Image } from "react-native";
 import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import AppText from "../components/AppText";
 import Screen from "../components/Screen";
+import { Formik } from "formik";
 import colors from "../config/colors";
 import defaultStyles from "../config/styles";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
   return (
     <Screen>
       <Image style={styles.image} source={require("../assets/logo-red.png")} />
-      <View style={styles.fiedlsContainer}>
-        <AppTextInput
-          autoFocus={true}
-          autoCapitalize="none"
-          autoCorrect={false}
-          onChangeText={(text) => setEmail(text)}
-          icon="email"
-          placeholder="Email"
-          keyboardType="email-address"
-        />
-        <AppTextInput
-          onChangeText={(text) => setPassword(text)}
-          icon="lock"
-          secureTextEntry={true}
-          placeholder="Password"
-        />
-      </View>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <>
+            <View style={styles.fiedlsContainer}>
+              <AppTextInput
+                autoFocus={true}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={handleChange("password")}
+                icon="email"
+                placeholder="Email"
+                keyboardType="email-address"
+              />
+              <AppTextInput
+                onChangeText={handleChange("password")}
+                icon="lock"
+                secureTextEntry={true}
+                placeholder="Password"
+              />
+            </View>
 
-      <View style={styles.buttonContainer}>
-        <AppButton
-          title="Login"
-          onPress={() =>
-            console.log(
-              `Logged sucessfully
-              email: ${email}
-              password: ${password}`
-            )
-          }
-        />
-      </View>
+            <View style={styles.buttonContainer}>
+              <AppButton title="Login" onPress={handleSubmit} />
+            </View>
+          </>
+        )}
+      </Formik>
     </Screen>
   );
 };
