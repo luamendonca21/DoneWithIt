385870adbea7e18131734dbfce118a822a39a8d5
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-16 17:04:24 +0100

    Image Input Component

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Button, StyleSheet, Image } from "react-native";
-import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
+
 import Screen from "./app/components/Screen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
@@ -11,36 +10,21 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import AccountScreen from "./app/screens/AccountScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
+import ImageInput from "./app/components/ImageInput";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import defaultStyles from "./app/config/styles";
+
 export default function App() {
   const [imageUri, setImageUri] = useState();
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) alert("You need to enable permission to acess the library");
-  };
 
-  const selectImage = async () => {
-    try {
-      const { uri, cancelled } = await ImagePicker.launchImageLibraryAsync();
-      if (!cancelled) setImageUri(uri);
-    } catch (error) {
-      console.log("error reading an image", error);
-    }
-  };
-  // equals to componentDidMount
-  useEffect(() => {
-    requestPermission();
-  }, []);
   return (
     <Screen>
-      <Button title="Select image" onPress={selectImage}></Button>
-      <Image source={{ uri: imageUri }} style={styles.image}></Image>
+      <ImageInput
+        onChangeImage={(uri) => setImageUri(uri)}
+        imageUri={imageUri}
+      />
     </Screen>
   );
 }
-
-const styles = StyleSheet.create({
-  image: {
-    width: "100%",
-    height: "50%",
-  },
-});
---
+++ app/components/ImageInput.js
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import { View, StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
+
+import defaultStyles from "../config/styles";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+
+const ImageInput = ({ imageUri, onChangeImage }) => {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permission to acess the library");
+  };
+
+  const handlePress = () => {
+    !imageUri ? selectImage() : deleteImage();
+  };
+
+  const selectImage = async () => {
+    try {
+      const { uri, cancelled } = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!cancelled) onChangeImage(uri);
+    } catch (error) {
+      console.log("error reading an image", error);
+    }
+  };
+
+  const deleteImage = () => {
+    Alert.alert("Delete", "Are you sure you want do delete this image?", [
+      { text: "Yes", onPress: () => onChangeImage(null) },
+      { text: "No" },
+    ]);
+  };
+
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <View style={styles.container}>
+        {imageUri && (
+          <Image source={{ uri: imageUri }} style={styles.image}></Image>
+        )}
+        {!imageUri && (
+          <MaterialCommunityIcons
+            size={50}
+            color={defaultStyles.colors.medium}
+            name="camera"
+          />
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ImageInput;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
+  },
+  container: {
+    marginHorizontal: 10,
+    borderRadius: 15,
+    width: 100,
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: defaultStyles.colors.light,
+  },
+});
