b65fd19c9485cfe99a47f3a9c0fcdf9e9c8e033d
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-16 22:27:02 +0100

    creating a stack navigator

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Text } from "react-native";
 import Screen from "./app/components/Screen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
@@ -13,10 +13,33 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ImageInput from "./app/components/ImageInput";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import defaultStyles from "./app/config/styles";
 import ImageInputList from "./app/components/ImageInputList";
 
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
