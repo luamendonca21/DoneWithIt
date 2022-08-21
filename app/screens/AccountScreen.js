16613bd53276d62939916837af9bf7eb15e17064
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-21 22:02:04 +0100

    logout feature

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -8,7 +8,7 @@ import AuthContext from "../auth/context";
 import defaultStyles from "../config/styles";
 
 const AccountScreen = ({ navigation }) => {
-  const { user } = useContext(AuthContext);
+  const { user, setUser } = useContext(AuthContext);
 
   const menuItems = [
     {
@@ -71,6 +71,7 @@ const AccountScreen = ({ navigation }) => {
       <ListItem
         showChevrons={true}
         title="Log Out"
+        onPress={() => setUser(null)}
         IconComponent={
           <Icon
             name="logout"
