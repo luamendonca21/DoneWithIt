a5d9de1fcbca373cea585a322dd080e8e908fd6c
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-23 16:49:27 +0100

    registration

---
+++ app/api/users.js
@@ -0,0 +1,6 @@
+import client from "./client";
+
+const endpoint = "/users";
+const register = (userInfo) => client.post(endpoint, userInfo);
+
+export default { register };
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Screen from "../components/Screen";
 import { StyleSheet } from "react-native";
-import { AppFormField, SubmitButton, AppForm } from "../components/Forms";
+import {
+  AppFormField,
+  SubmitButton,
+  AppForm,
+  ErrorMessage,
+} from "../components/Forms";
 import * as Yup from "yup";
+import usersApi from "../api/users";
+import authApi from "../api/auth";
+import useAuth from "../auth/useAuth";
 
 const validateYupSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -10,13 +18,34 @@ const validateYupSchema = Yup.object().shape({
   password: Yup.string().required().min(4).label("Password"),
 });
 const RegisterScreen = () => {
+  const { logIn } = useAuth();
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (userInfo) => {
+    const result = await usersApi.register(userInfo);
+    if (!result.ok) {
+      if (result.data) setError(result.data.error);
+      else {
+        setError("An unexpected error occurred.");
+        console.log(result);
+      }
+      return;
+    }
+
+    const { data: authToken } = await authApi.login(
+      userInfo.email,
+      userInfo.password
+    );
+    logIn(authToken);
+  };
   return (
     <Screen style={styles.container}>
       <AppForm
         initialValues={{ name: "", email: "", password: "" }}
         validationSchema={validateYupSchema}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
       >
+        <ErrorMessage error={error} visible={error} />
         <AppFormField name="name" icon="account-circle" placeholder="Name" />
         <AppFormField
           autoCapitalize="none"
