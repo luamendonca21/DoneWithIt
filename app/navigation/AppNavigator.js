cd12392c3bd7d3365e4857924793b1b43996c885
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-24 15:01:55 +0100

    notifications - getting a push token

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import ListingEditScreen from "../screens/ListingEditScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
 
+import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import NewListingButton from "./NewListingButton";
 import routes from "../navigation/routes";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
+  useEffect(() => {
+    registerForPushNotifications();
+  }),
+    [];
+  const registerForPushNotifications = async () => {
+    try {
+      const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (!permission.granted) return;
+
+      const token = await Notifications.getExpoPushTokenAsync();
+      console.log(token);
+    } catch (error) {
+      console.log("Error getting a push token", error);
+    }
+  };
   return (
     <Tab.Navigator>
       <Tab.Screen
