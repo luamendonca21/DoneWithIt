9703b41715c77ee8ab5ffec78526133ae81fd73a
Luana Mendonça <luamendonca_2000@hotmail.com>
2022-08-24 18:55:20 +0100

    keyboard fix

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,31 +1,37 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, KeyboardAvoidingView } from "react-native";
 import AppText from "../components/AppText";
 import Screen from "../components/Screen";
 import defaultStyles from "../config/styles";
 import { ListItem } from "../components/Lists";
 import { Image } from "react-native-expo-image-cache";
 import ContactSellerForm from "../components/Forms/ContactSellerForm";
+import { Platform } from "react-native";
 
 const ListingDetailsScreen = ({ route }) => {
   const listing = route.params;
   return (
     <Screen>
-      <View style={styles.listingContainer}>
-        <Image style={styles.image} uri={listing.images[0].url} />
-        <View style={styles.detailsContainer}>
-          <AppText style={styles.title}>{listing.title}</AppText>
-          <AppText style={styles.price}> {listing.price} €</AppText>
-          <View style={styles.container}>
-            <ListItem
-              title="Luana Mendonça"
-              subTitle="5 Listings"
-              image={require("../assets/luana.jpg")}
-            />
+      <KeyboardAvoidingView
+        behavior="position"
+        keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 100}
+      >
+        <View style={styles.listingContainer}>
+          <Image style={styles.image} uri={listing.images[0].url} />
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{listing.title}</AppText>
+            <AppText style={styles.price}> {listing.price} €</AppText>
+            <View style={styles.container}>
+              <ListItem
+                title="Luana Mendonça"
+                subTitle="5 Listings"
+                image={require("../assets/luana.jpg")}
+              />
+            </View>
           </View>
+          <ContactSellerForm listing={listing} />
         </View>
-        <ContactSellerForm listing={listing} />
-      </View>
+      </KeyboardAvoidingView>
     </Screen>
   );
 };
